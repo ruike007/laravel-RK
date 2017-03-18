ac75e5603b3be19b9c922bb38a38479a6456146c
ruike007 <914980770@qq.com>
2017-03-18 17:32:30 +0800

    首页设置

---
+++ app/Discussion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    //
+    protected $fillable = [
+        'title', 'body', 'user_id','last_user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);//$discussion->user
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+        <meta charset = "utf-8">
+        <title>Laravel App</title>
+        <link rel="stylesheet" href="/css/bootstrap.css">
+</head>
+
+<body>
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel App</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">主页</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../navbar/">登录</a></li>
+                <li><a href="../navbar-fixed-top/">注册</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+    @yield('content')
+</body>
+
+</html>
